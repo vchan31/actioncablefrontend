c4bfd79efbf1bdc6b2c3661a34513f856b3491c9
Vincent Chan <vchan313@gmail.com>
2019-04-26 13:30:21 -0400

    starting the code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Input } from 'semantic-ui-react'
+
 
 class App extends Component {
+
+  state = {
+    tweets: [],
+    addTweet: "",
+    newTweetToDisplay: false
+  }
+
+  handleInputOnChange = (e) => {
+    this.setState({
+      addTweet: e.target.value
+    })
+  }
+
+  handleAddTweet = (e) => {
+    e.preventDefault()
+    console.log('submitted a tweet!')
+
+  }
+
   render() {
+    console.log(this.state)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+          <form onSubmit={(e)=>this.handleAddTweet(e)}>
+          <div class="ui huge icon input"><input value={this.state.addTweet} type="text" placeholder="Search..." onChange={(e)=>this.handleInputOnChange(e)}/>
+          <button class="ui button">Add Tweet</button></div>
+          </form>
+
+
       </div>
     );
   }
